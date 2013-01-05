6d8dc37554129c9a346cb50fca11ff4cead90731
Leooo <viet_lionel@yahoo.fr>
2013-01-05 13:42:01 +0000

    First commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Leo360RoR1::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leo360RoR1::Application.config.secret_token = '2948b5783c6be2d83f4e68fae95122be907ac7533665cecafc2b47dd1237e0fa93236e75554d34e1be9241a7a5f45a10c1db5b4bbf86a8f74102c396ad2031f6'
