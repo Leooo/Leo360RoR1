4de37675e0b69285b828e191073e73ea0450b4c8
Leooo <viet_lionel@yahoo.fr>
2013-01-05 16:15:54 +0000

    end chap

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,51 +1,40 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  subject { page }
 
   describe "Home page" do
+    before { visit root_path } 
 
-    it "should have the h1 '360'" do
+    #it "should have the h1 '360'" do
       #visit '/static_pages/home'
-      visit root_path
-      page.should have_selector('h1', :text => "360")
-    end
+      #visit root_path
+    it { should have_selector('h1', text: "360") }
+    #end
 
-    it "should have the title '360 | Home'" do
-      visit root_path
-      page.should have_selector('title', :text => "360 | Home")
-    end
+    it { should have_selector('title', :text => "360 | Home") }
 
   end
 
   describe "Help page" do
-
-    it "should have the content 'Help'" do
-      visit help_path
-      page.should have_content("Help")
-    end
+    before {visit help_path} 
+      
+    it {should have_content("Help")}
 
   end
 
   describe "About Us page" do
+    before {visit about_path} 
 
-    it "should have the content 'About Us'" do
-      visit about_path
-      page.should have_content("About Us")
-    end
+    it {should have_content("About Us")}
 
   end
 
   describe "Contact page" do
+    before {visit contact_path} 
 
-    it "should have the h1 'Contact'" do
-      visit contact_path
-      page.should have_selector('h1', :text => "Contact")
-    end
-
-    it "should have the title '360 | Contact'" do
-      visit contact_path
-      page.should have_selector('title', :text => "360 | Contact")
-    end
+    it {should have_selector('h1', :text => "Contact")}
+    it {should have_selector('title', :text => "360 | Contact")}
 
   end
 
