c00dcded857e20da4c936f0c7c4596a9a89ea304
Leooo <viet_lionel@yahoo.fr>
2013-01-06 10:37:59 +0000

    Finished sign in out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,7 +5,7 @@ module SessionsHelper
     self.current_user=user
   end
 
-  def sign_in?
+  def signed_in?
     !current_user.nil?
   end
 
@@ -15,6 +15,11 @@ module SessionsHelper
 
   def current_user
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+  end
+
+  def sign_out
+    self.current_user=nil
+    cookies.delete(:remember_token)
   end
 
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -17,15 +17,35 @@ describe "UserPages" do
     end
 
     describe "with valid information" do
+      
       before do
         fill_in "Name",           with: "Example user"
         fill_in "Email",          with: "user@example.com"
         fill_in "Password",       with: "foobar"
         fill_in "Password Confirmation",       with: "foobar"
       end
+
       it "should create a user" do
         expect {click_button submit}.to change(User, :count).by(1)
       end
+
+      describe "after saving the user" do
+
+        before {click_button submit}
+        let (:user) {User.find_by_email('user@example.com')}
+        it {should have_selector('title',text:user.name)}
+        it {should have_selector('div.alert.alert-success',text:'Welcome')}
+        it {should have_link('Sign out')}
+
+        describe "followed by signout" do
+
+          before {click_link "Sign out"}
+          it {should have_link ('Sign in')}
+
+        end
+
+      end
+
     end
 
   end
